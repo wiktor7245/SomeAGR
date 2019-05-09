16753de239e1ce10d38c3a748462f64365fa0224
wiktor7245 <wiktor7245@o2.pl>
2019-05-09 08:53:40 +0200

    dasd

--- untitled/04_TylmanWiktor_cz.py
+++ untitled/04_TylmanWiktor_cz.py
@@ -1,86 +1,71 @@
-# 2a
-print()
-print("Zadanie 3: ")
-print()
-
 import math
 
-# ODTWARZANIE STRUKTURY GRAFU
-file = open("FindForest.txt", "r")
-lines = file.read().splitlines()
+f = open("FindForest.txt", "r")
+l= f.read().splitlines()
 
 graph = {}
 i = 1
-for line in lines:
-	splittedLine = line.split()
+for line in l:
+	newLine = line.split()
 	graph[i] = {}
 	j = 1
-	for weight in splittedLine:
+	for weight in newLine:
 		if weight != '-' and j != i:
 			graph[i][j] = int(weight)
 		j += 1
 	i += 1
 
-#------------------------------------------------------------------------------#
+def findforest():
+	# (previous, distance)
+	dist_tuple = {}
+	#there was sth wrong and i had to give math.inf on the end
+	for i in range(1, len(graph) + 1):
+		dist_tuple[i] = (0, math.inf)
 
-def prim_tree():
-	# INICJALIZACJA TABELI POMOCNICZEJ
-	table_data = {} # SŁOWNIK TUPLI (POPRZEDNIK, DYSTANS)
-	# ZE WZGLĘDU NA OGRANICZENIA ALGORYTMU
-	# ODLEGŁOŚCI KORZENI DO SIEBIE SAMYCH (0)
-	# W KOŃCOWYM REZULTACIE ZAPISANE SĄ JAKO MATH.INF
-	for index in range(1, len(graph) + 1):
-		table_data[index] = (0, math.inf)
-
-	# ZAPISANIE NIEODWIEDZONYCH WIERZCHOŁKÓW DO LISTY
-	# ORAZ IMPLEMENTACJA LISTY SKŁADOWYCH
 	not_visited = list(range(1, len(graph) + 1))
-	components_index = -1
-	components_vertices = []
+	com_i = -1
+	com_v = []
 	while not_visited:
-		# nv == not_visited
-		nv_table_data = [d for d in table_data.items() if d[0] in not_visited]
-		sorted_nv_table_data = sorted(nv_table_data, key = lambda d:d[1][1])
-		first_nv = sorted_nv_table_data[0][0]
-		not_appended_neighbours = [item for item in graph[first_nv].items()\
+		# nv == not_visitedd
+		not_visitedd = [d for d in dist_tuple.items() if d[0] in not_visited]
+		sort_not_visitedd = sorted(not_visitedd, key = lambda d:d[1][1])
+		first_not_visitedd = sort_not_visitedd[0][0]
+		not_added_neigh = [item for item in graph[first_not_visitedd].items()\
 		if item[0] in not_visited]
-		for adjacent_vertex, distance in not_appended_neighbours:
-			if table_data[adjacent_vertex][1] > distance:
-				table_data[adjacent_vertex] = (first_nv, distance)
-		not_visited.remove(first_nv)
+		for adj_v, distance in not_added_neigh:
+			if dist_tuple[adj_v][1] > distance:
+				dist_tuple[adj_v] = (first_not_visitedd, distance)
+		not_visited.remove(first_not_visitedd)
 
-		# JEŚLI WSZYSCY SĄSIEDZI SĄ NIEODWIEDZENI MAMY KOLEJNĄ SKŁADOWĄ
-		visited_adjacent_vertices = [v for v in graph[first_nv].keys()\
+		visited_adj_v = [v for v in graph[first_not_visitedd].keys()\
 		if v not in not_visited]
-		if not visited_adjacent_vertices:
-			components_index += 1
-			components_vertices.append([])
+		if not visited_adj_v:
+			com_i += 1
+			com_v.append([])
 
-		components_vertices[components_index].append(first_nv)
+		com_v[com_i].append(first_not_visitedd)
 
-	print("WYKORZYSTANA METODA: III")
-	components_edges = []
+	print("Wykorzystana metoda: III")
+	com_e = []
 	forest_weight = 0
 	index = 0
-	for component_vertices in components_vertices:
-		components_edges.append([])
+	for com_ver in com_v:
+		com_e.append([])
 		weight = 0
 
-		for vertex in component_vertices:
-			if table_data[vertex][0] != 0 and\
-			(table_data[vertex][0], vertex) not in components_edges[index]:
-				components_edges[index].append((vertex, table_data[vertex][0]))
-				weight += table_data[vertex][1]
+		for vertex in com_ver:
+			if dist_tuple[vertex][0] != 0 and\
+			(dist_tuple[vertex][0], vertex) not in com_e[index]:
+				com_e[index].append((vertex, dist_tuple[vertex][0]))
+				weight += dist_tuple[vertex][1]
 
 		forest_weight += weight
 		index += 1
 		print("DRZEWO " + str(index))
-		print("Wierzchołki: " + str(sorted(component_vertices)))
-		print("Krawędzie: " + str(components_edges[index - 1]))
+		print("Wierzchołki: " + str(sorted(com_ver)))
+		print("Krawędzie: " + str(com_e[index - 1]))
 		print("Waga drzewa: " + str(weight))
 		print()
-	print("Waga lasu: " + str(forest_weight))
+		print("Waga lasu: " + str(forest_weight))
 
-#------------------------------------------------------------------------------#
-
-prim_tree()
+findforest()
